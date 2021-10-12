6de71ad1a594c4429006feaf2ddcf8ca684eacc9
uwadonat <uwadonat@gmail.com>
2021-10-12 19:41:19 +0300

    Configure store

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,18 +1,23 @@
 import React from "react"
-
-import PropTypes from "prop-types"
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { Provider } from 'react-redux'
+
 import HelloWord from "./HelloWord"
+import configureStore from '../configureStore'
+const store = configureStore();
+
 class App extends React.Component {
   render () {
     return (
-      
-          <BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
             <Switch>
               {/* <Route exact path="/" render={() => ("Home!")} /> */}
               <Route path="/" render={() => <HelloWord  greeting="Friend"/>} />
             </Switch>
           </BrowserRouter>
+      </Provider>
+          
       
     );
   }
--- app/javascript/components/HelloWord.js
+++ app/javascript/components/HelloWord.js
@@ -1,16 +1,40 @@
 import React from "react"
-import PropTypes from "prop-types"
+import { connect } from "react-redux";
+import { createStructuredSelector } from 'reselect';
+
+const GET_MESSAGES_REQUEST = 'GET_MESSAGES_REQUEST';
+import PropTypes from "prop-types";
+
+function getMessages() {
+  console.log('getMessages() Action!!')
+  return {
+    type: GET_MESSAGES_REQUEST
+  };
+};
+
 class HelloWord extends React.Component {
   render () {
     return (
       <React.Fragment>
         Greeting: {this.props.greeting}
+        <button className="getMessageBtn" onClick={() => this.props.getMessages()}>getMessages</button>
+        <br />
+        <ul> { MessagesList }</ul>
       </React.Fragment>
     );
   }
 }
 
+const structuredSelector = createStructuredSelector({
+  messages: state => state.messages,
+});
+
 HelloWord.propTypes = {
   greeting: PropTypes.string
 };
-export default HelloWord
+
+const mapDispatchToProps = { getMessages };
+
+
+export default connect(structuredSelector, mapDispatchToProps)(HelloWord);
+
